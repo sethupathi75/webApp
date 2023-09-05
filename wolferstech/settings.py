355f9a57d29869e7d0a37a71978d86ff632a49fe
sethupathi75 <83010699+sethupathi75@users.noreply.github.com>
2023-09-05 08:03:02 +0000

    #

--- wolferstech/settings.py
+++ wolferstech/settings.py
@@ -26,7 +26,7 @@ STATIC_DIR=os.path.join(BASE_DIR1,'static')
 SECRET_KEY = 'django-insecure-98^$v8iy9kar0b9ls$+6%2588f=&^3aj9sfa&a$_7=abm2$^^*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
